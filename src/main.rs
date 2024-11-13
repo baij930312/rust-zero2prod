bc1a53bdd4a024e18487c2fda6490cfe5ccf62be
baijin <baij930312@163.com>
2024-11-13 23:43:11 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use ::actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::HttpResponse;
+
+async fn health_check(_req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
